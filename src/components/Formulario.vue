3b1b501a68bbfc470fe05b6d64c285f1bccc89a1
oc-garcia <octaviogarcia1988@gmail.com>
2023-07-29 14:23:05 -0300

    Finished first module

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,15 +7,15 @@
       <div class="column">
         <div class="is-flex is-align-items-center is-justify-content-space-between">
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -31,5 +31,26 @@
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "Formulario",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+    };
+  },
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
+    },
+  },
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos++;
+      }, 1000);
+    },
+    finalizar() {
+      clearInterval(this.cronometro)
+    },
+  },
 });
 </script>
